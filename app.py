71e3b138d77d3d7fae5ffddc622cf586dcf17299
Deiferson da Silva Moura <deifersondasilvamoura@gmail.com>
2022-12-17 01:56:49 -0300

    feat: aducuiba sudebar

--- app.py
+++ app.py
@@ -1,54 +1,66 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from streamlit_option_menu import option_menu
 
 
-st.title('Uber pickups in NYC')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+# 1. as sidebar menu
+with st.sidebar:
+    selected = option_menu("Menu", ["Home", 'Sobre mim'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=0)
+    
+
+if selected == "Home" :
+    st.title('Uber pickups in NYC')
+
+
+    DATE_COLUMN = 'date/time'
+    DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 
+    @st.cache
+    def load_data(nrows):
+        data = pd.read_csv(DATA_URL, nrows=nrows)
+        lowercase = lambda x: str(x).lower()
+        data.rename(lowercase, axis='columns', inplace=True)
+        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+        return data
 
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    # Create a text element and let the reader know the data is loading.
+    data_load_state = st.text('Loading data...')
+    # Load 10,000 rows of data into the dataframe.
+
+    data = load_data(1000)
+    # Notify the reader that the data was successfully loaded.
+    data_load_state.text("Done! (using st.cache)")
+
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(data)
+
+    number_of_pickups_by_hour = st.checkbox('Show Number of pickups by hour')
 
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
+    if number_of_pickups_by_hour:
+        st.subheader('Number of pickups by hour')
+        hist_values = np.histogram(
+            data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
-data = load_data(1000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-number_of_pickups_by_hour = st.checkbox('Show Number of pickups by hour')
+        st.bar_chart(hist_values)    
 
 
-if number_of_pickups_by_hour:
-    st.subheader('Number of pickups by hour')
-    hist_values = np.histogram(
-        data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
 
-    st.bar_chart(hist_values)    
+    st.subheader('Map of all pickups')
+
+    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+    st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+    st.map(filtered_data)
+
     
-
-hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-
-st.subheader('Map of all pickups')
-
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
-
+if selected == "Sobre mim" :
+    st.title('Sobre mim')
+    
